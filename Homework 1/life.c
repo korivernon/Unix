cc0dc38a8987ee4540484643540cb2f214833287
Kori Vernon <korivernon@nyu.edu>
2021-02-08 11:10:33 -0500

    issue in initializing board

--- Homework 1/life.c
+++ Homework 1/life.c
@@ -2,57 +2,164 @@
 #include <string.h>
 #include <stdlib.h>
 
-//evolve function
-
-//printboardfunction -> also prints generation
-
 //write function to file...??
 
-//figure out better way to .. store board
-//https://stackoverflow.com/questions/2614249/dynamic-memory-for-2d-char-array
-void loadLife(FILE *fptr, int row, int col, int gen){
+void initBoard(char **board, int col)
+{
+    int i;
+    for (i = 0; i < col; i++)
+    {
+        board[i] = malloc(col * sizeof(char));
+    }
+}
+
+void printBoard(char **board, int row, int col, int currGen)
+{
+    int i, j;
+    printf("Generation: %d\n", currGen);
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            printf("%c", board[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void checkNeighbors(char **board, int i, int j, int neigh)
+{
+    //check top
+    if (i > 0 && board[i - 1][j] == '*')
+    {
+        neigh++;
+    }
+    //check left
+    if (j > 0 && board[i][j - 1] == '*')
+    {
+        neigh++;
+    }
+    //check right
+    if (j < 9 && board[i][j + 1] == '*')
+    {
+        neigh++;
+    }
+    //check bottom
+    if (i < 9 && board[i + 1][j] == '*')
+    {
+        neigh++;
+    }
+    //check left upper diagonal
+    if ((i > 0 && j > 0) && board[i - 1][j - 1] == '*')
+    {
+        neigh++;
+    }
+    //check right upper diagonal
+    if ((i > 0 && j < 9) && board[i - 1][j + 1] == '*')
+    {
+        neigh++;
+    }
+    //check left lower digonal
+    if ((i < 9 && j > 0) && board[i + 1][j - 1] == '*')
+    {
+        neigh++;
+    }
+    //check right lower diagonal
+    if ((i < 9 && j < 9) && board[i + 1][j + 1] == '*')
+    {
+        neigh++;
+    }
+    printf("neighbors in cycle: %d\n", neigh);
+}
+void evolve(char **board, int row, int col, int currGen)
+{
+    printBoard(board, row, col, currGen);
+    int i, j, neigh;
+
+    char **temp = malloc(row * sizeof(char *));
+    initBoard(temp, col);
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            neigh = 0;
+            checkNeighbors(board, i, j, neigh);
+            if (board[i][j] == '-')
+            {
+                if (neigh == 3)
+                {
+                    temp[i][j] = '*';
+                }
+                else
+                {
+                    temp[i][j] = board[i][j];
+                }
+            }
+            else if (board[i][j] == '*')
+            {
+                if (neigh < 2 || neigh < 3)
+                {
+                    board[i][j] = '-';
+                }
+                else
+                {
+                    temp[i][j] = board[i][j];
+                }
+            }
+        }
+    }
+    board = temp;
+    free(board);
+}
+
+void loadLife(FILE *fptr, int row, int col, int gen)
+{
     /* Load the game of life with the number of columns, then
      * output the game into the struct called board
      */
     //printf("row: %d, col: %d, gen: %d\n", row, col, gen);
 
-    char grid[row+1][col+1];
-    char ch = getc(fptr);
-    
+    char **board = malloc(row * sizeof(char *));
+    initBoard(board, col);
+
     char line[col]; //max linelength
     int currRow = 0;
-    while (fgets(line, sizeof(line), fptr)) {
-        int i; 
-        for( i  = 0; i < col; i++){
-            if (line[i] == ' '){
-                grid[currRow][i] = '-';
-                printf("%c", grid[currRow][i]);
+    while (fgets(line, sizeof(line), fptr))
+    {
+        int i;
+        for (i = 0; i < col; i++)
+        {
+            if (line[i] == '*')
+            {
+                board[currRow][i] = '*';
             }
-            else if (line[i] == '*'){
-                grid[currRow][i] = '*';
-                printf("%c", grid[currRow][i]);
-            }
-            else{
-                grid[currRow][i] = '-';
-                printf("%c", grid[currRow][i]);
+            else
+            {
+                board[currRow][i] = '-';
             }
         }
-        grid[currRow][i] = '\n';
-        printf("%c", grid[currRow][i]);
         currRow++;
     }
     // continue filling rows
-    while (currRow < row){
+    while (currRow < row)
+    {
         int i;
-        for(i= 0; i < col; i++){
-            grid[currRow][i] = '-';
-            printf("%c", grid[currRow][i]);
+        for (i = 0; i < col; i++)
+        {
+            board[currRow][i] = '-';
         }
-        grid[currRow][i] = '\n';
-        printf("%c", grid[currRow][i]);
         currRow++;
     }
+    /*
+    for (int currGen = 0; currGen < gen; currGen++)
+    {
+        evolve(board, row, col, currGen);
+    }
+*/
+    printBoard(board, row, col, 0);
     fclose(fptr);
+    free(board);
 }
 
 /*
@@ -60,8 +167,10 @@ void loadLife(FILE *fptr, int row, int col, int gen){
  * and a char pointer and check to see if that file exists.
  * If the file does exist, it will open that file
  */
-void checkFile(FILE *fptr){
-    if (fptr == NULL){
+void checkFile(FILE *fptr)
+{
+    if (fptr == NULL)
+    {
         printf("life: error, cannot read file\n");
         exit(0);
     }
@@ -70,7 +179,8 @@ void checkFile(FILE *fptr){
  * This will convert a single integer
  * that we need for the function
  */
-void convSingleInt(char * arg, int * hold){
+void convSingleInt(char *arg, int *hold)
+{
     char c[512];
     strcpy(c, arg);
     char *num = c;
@@ -81,36 +191,43 @@ void convSingleInt(char * arg, int * hold){
 /* decision fork function will assign correct values
  * and take the correct path to complete task
  */
-void decisionFork(int argc, char *argv[]){
+void decisionFork(int argc, char *argv[])
+{
     int row = 10, col = 10, gen = 10;
     FILE *fptr;
-    if (argc == 1){
+    if (argc == 1)
+    {
         fptr = fopen("life.txt", "r");
-        loadLife(fptr,  row, col, gen);
+        loadLife(fptr, row, col, gen);
     }
-    else if (argc == 3){
+    else if (argc == 3)
+    {
         fptr = fopen("life.txt", "r");
         convSingleInt(argv[1], &row);
         convSingleInt(argv[2], &col);
         loadLife(fptr, row, col, gen);
     }
-    else if (argc == 4){
+    else if (argc == 4)
+    {
         fptr = fopen(argv[3], "r");
         checkFile(fptr);
         convSingleInt(argv[1], &row);
         convSingleInt(argv[2], &col);
         loadLife(fptr, row, col, gen);
     }
-    else if (argc == 5){
+    else if (argc == 5)
+    {
         fptr = fopen(argv[3], "r");
         checkFile(fptr);
         convSingleInt(argv[1], &row);
         convSingleInt(argv[2], &row);
         convSingleInt(argv[4], &gen);
-        loadLife(fptr,  row, col, gen);
+        loadLife(fptr, row, col, gen);
     }
-    else {
-        for (int i = 0; i < argc; i++){
+    else
+    {
+        for (int i = 0; i < argc; i++)
+        {
             printf("%d input: %s\n", i, argv[i]);
         }
 
@@ -118,12 +235,12 @@ void decisionFork(int argc, char *argv[]){
         exit(1);
     }
 }
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     /*
      * Here we will split and determine what happens.
      * rows, columns, filename, and generations will be defaulted
      * to original values.
      */
-    printf("num of arguments is: %d\n", argc);
     decisionFork(argc, argv);
 }
