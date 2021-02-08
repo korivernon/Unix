f491bb9022e896821dee2d5341c9f4dfb7ff7eb6
Kori Vernon <korivernon@nyu.edu>
2021-02-08 16:21:53 -0500

    modified directory

--- Homework 1/life.c
+++ Homework 1/life.c
@@ -2,8 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
-//write function to file...??
+// Constant
+char OUT[] = "output.txt";
 
+/*
+ * The initBoard function will take in a char** and an amoount
+ * of column and create a two dimensional character array. 
+ */
 void initBoard(char **board, int col)
 {
     int i;
@@ -12,7 +17,11 @@ void initBoard(char **board, int col)
         board[i] = malloc(col * sizeof(char));
     }
 }
-
+/*
+ * The printBoard function will take in the row, col, game board,
+ * along with the current generation number and will print out
+ * the contents to the command line. 
+ */
 void printBoard(char **board, int row, int col, int currGen)
 {
     int i, j;
@@ -27,73 +36,93 @@ void printBoard(char **board, int row, int col, int currGen)
     }
 }
 
-void checkNeighbors(char **board, int i, int j, int neigh)
+/* This function prints the board to the file, which upon further
+ * analysis... doesn't actually need to be created
+ */
+void printBoardToFile(FILE *outfile, char **board, int row, int col, int currGen)
+{
+    int i, j;
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            fputc(board[i][j], outfile);
+        }
+        fputc('\n', outfile);
+    }
+}
+
+void checkNeighbors(char **board, int x, int y, int neigh, int maxRow, int maxCol)
 {
     //check top
-    if (i > 0 && board[i - 1][j] == '*')
+    if (x > 0 && board[x - 1][y] == '*')
     {
         neigh++;
     }
     //check left
-    if (j > 0 && board[i][j - 1] == '*')
+    if (y > 0 && board[x][j - 1] == '*')
     {
         neigh++;
     }
     //check right
-    if (j < 9 && board[i][j + 1] == '*')
+    if (y < maxCol && board[x][y + 1] == '*')
     {
         neigh++;
     }
     //check bottom
-    if (i < 9 && board[i + 1][j] == '*')
+    if (x < maxRow && board[x + 1][y] == '*')
     {
         neigh++;
     }
     //check left upper diagonal
-    if ((i > 0 && j > 0) && board[i - 1][j - 1] == '*')
+    if ((x > 0 && y > 0) && board[x - 1][y - 1] == '*')
     {
         neigh++;
     }
     //check right upper diagonal
-    if ((i > 0 && j < 9) && board[i - 1][j + 1] == '*')
+    if ((x > 0 && y < maxCol) && board[x - 1][y + 1] == '*')
     {
         neigh++;
     }
     //check left lower digonal
-    if ((i < 9 && j > 0) && board[i + 1][j - 1] == '*')
+    if ((x < maxRow && y > 0) && board[x + 1][y - 1] == '*')
     {
         neigh++;
     }
     //check right lower diagonal
-    if ((i < 9 && j < 9) && board[i + 1][j + 1] == '*')
+    if ((x < maxRow && y < maxCol) && board[x + 1][y + 1] == '*')
     {
         neigh++;
     }
     printf("neighbors in cycle: %d\n", neigh);
 }
-void evolve(char **board, int row, int col, int currGen)
+
+void evolve(char **board, int row, int col, int currGen, FILE *outfile)
 {
     printBoard(board, row, col, currGen);
+    //printBoardToFile(outfile, board, row, col, currGen); // not needed upon further analysis
+
     int i, j, neigh;
 
-    char **temp = malloc(row * sizeof(char *));
-    initBoard(temp, col);
+    char **nextGen = malloc(row * sizeof(char *));
+    initBoard(nextGen, col);
+    //temp=>nextGen
 
     for (i = 0; i < row; i++)
     {
         for (j = 0; j < col; j++)
         {
             neigh = 0;
-            checkNeighbors(board, i, j, neigh);
+            checkNeighbors(board, i, j, neigh, row, col);
             if (board[i][j] == '-')
             {
                 if (neigh == 3)
                 {
-                    temp[i][j] = '*';
+                    nextGen[i][j] = '*';
                 }
                 else
                 {
-                    temp[i][j] = board[i][j];
+                    nextGen[i][j] = board[i][j];
                 }
             }
             else if (board[i][j] == '*')
@@ -104,33 +133,37 @@ void evolve(char **board, int row, int col, int currGen)
                 }
                 else
                 {
-                    temp[i][j] = board[i][j];
+                    nextGen[i][j] = board[i][j];
                 }
             }
         }
     }
-    board = temp;
+    board = nextGen;
     free(board);
 }
 
 void loadLife(FILE *fptr, int row, int col, int gen)
 {
-    /* Load the game of life with the number of columns, then
-     * output the game into the struct called board
+    /* Load the game of life with the number of rows and columns
+     * then run game.
      */
-    //printf("row: %d, col: %d, gen: %d\n", row, col, gen);
-
+    //initialize the board
     char **board = malloc(row * sizeof(char *));
     initBoard(board, col);
+    // prepare to read lines in to the maximum column
+    char line[col];
+    // initialize integer variables
+    int currRow = 0, i;
+    //FILE *outfile; // not needed upon further analysis
+    //outfile = fopen(OUT, "w"); // not needed upon further analysis
 
-    char line[col]; //max linelength
-    int currRow = 0;
     while (fgets(line, sizeof(line), fptr))
-    {
-        int i;
+    { // read ptr is going to be where you finish the read
+        //may have to dump characters..
+        //getline??
         for (i = 0; i < col; i++)
         {
-            if (line[i] == '*')
+            if (line[i] == '*') //this may not exist
             {
                 board[currRow][i] = '*';
             }
@@ -141,7 +174,7 @@ void loadLife(FILE *fptr, int row, int col, int gen)
         }
         currRow++;
     }
-    // continue filling rows
+    // continue filling rows if the file ends prematurely
     while (currRow < row)
     {
         int i;
@@ -151,14 +184,17 @@ void loadLife(FILE *fptr, int row, int col, int gen)
         }
         currRow++;
     }
-    /*
+
     for (int currGen = 0; currGen < gen; currGen++)
     {
-        evolve(board, row, col, currGen);
+        evolve(board, row, col, currGen, outfile);
     }
-*/
-    printBoard(board, row, col, 0);
+    // printBoard(board, row, col, 0);
+    // close used files
     fclose(fptr);
+
+    //fclose(outfile); // this is not needed upon further analysis -> stdout
+    //close board
     free(board);
 }
 
@@ -172,7 +208,7 @@ void checkFile(FILE *fptr)
     if (fptr == NULL)
     {
         printf("life: error, cannot read file\n");
-        exit(0);
+        exit(1); // 0 implies everything went fine
     }
 }
 /*
@@ -181,6 +217,7 @@ void checkFile(FILE *fptr)
  */
 void convSingleInt(char *arg, int *hold)
 {
+    // convert to strtol...
     char c[512];
     strcpy(c, arg);
     char *num = c;
