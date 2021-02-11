5dffae9f9c7a7fc28af67d8c46d416cdf080fdfd
Kori Vernon <korivernon@nyu.edu>
2021-02-11 15:44:27 -0500

    latest

--- Lecture 4/execing.c
+++ Lecture 4/execing.c
@@ -4,6 +4,20 @@
 /* beginning of 2/11/21 lecture 
  * replaces current process image with a new
  * process image.
+ *
+ * exec is only going to return if exec fails!
+ * once exec has replaced the current memory image with
+ * the present memory image, then it will not return.
+ *
+ * perhaps you did not have executable permission... if
+ * exec fails. 
+ *
+ * the notion of "When do we come back from exec" does
+ * not exist; the only time we come back from exec is
+ * if we get a return statement.
+ *
+ * exec always returns -1 -> indicates success.. you
+ * do not have to check return value
  */
 
 int main(){
