a31b3197512156068bdac12c0676ea42594b8c1f
Kori Vernon <korivernon@nyu.edu>
2021-03-18 08:57:27 -0400

    all

---
+++ Homework 4/shell.c
@@ -0,0 +1,51 @@
+#include <stdio.h> 
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUF_LEN 80
+#define TRUE 1
+#define EXITCOND 0
+#define EX exit\n
+
+void print(char * command){
+    int len = strlen(command);
+    printf("%s\n" , command);
+}
+void runShell(void){
+    while(TRUE){
+        printf("trapshell\n$ ");
+        char command[BUF_LEN]; // buffer line
+        
+        if (!fgets(command, BUF_LEN, stdin)) {
+            break;
+        }
+        int bcond = 0;
+        
+        if (strcmp(command, "exit\n") == EXITCOND || strcmp(command, "exit\0") == EXITCOND){
+            break;
+        } // if the line is exit\n then exit
+        // split command and return into argv
+        print(command);
+        int pid = fork();
+        int len = strlen(command);
+        if (command[len-1] == '\n'){
+            command[len-1] = '\0';
+        }
+        if (pid == 0){ // child  process
+            execlp(command, command, NULL);
+            fprintf(stderr, "execlp err: child process could not execlp\n\n");
+            break;
+        }
+        else{
+            wait(0); // parent process
+        }
+    }
+}
+
+int main(void){
+    runShell();
+    return 0;
+}
